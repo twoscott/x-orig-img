a68b7c19af6cf16453a565bab748b4d586903fb8
Scotto <twoscott99@gmail.com>
2018-06-03 21:23:02 +0100

    Add files via upload
    inefficient at replacing extension but will improve soonTM

---
+++ background.js
@@ -0,0 +1,30 @@
+var browser = chrome;
+if (!chrome){
+	browser = browser;
+}
+
+extension_replacements = {".jpg:orig"  : ".jpg", ".png:orig"  : ".png",
+                          ".jpg:large" : ".jpg", ".png:large" : ".png",
+                          ".jpg:medium": ".jpg", ".png:medium": ".png",
+                          ".jpg:small" : ".jpg", ".png:small" : ".png",
+                          ".jpg:tiny"  : ".jpg", ".png:tiny"  : ".png"}
+
+function multiReplace(string_var, replace_pairs) {
+    for (let [key, val] of Object.entries(replace_pairs)) {
+        string_var = string_var.replace(key, val);
+    }
+    return string_var
+}
+
+browser.webRequest.onHeadersReceived.addListener(details => {
+    browser.extension.getBackgroundPage().console.log(details);
+    let url_split = details.url.split("/"); filename = url_split[url_split.length - 1];
+    let fixed_filename = multiReplace(filename, extension_replacements);
+    details.responseHeaders.push({name: "content-disposition", value: "inline; filename=\"" + fixed_filename + "\";"});
+
+    return {responseHeaders: details.responseHeaders}
+},
+{
+    urls: ["*://pbs.twimg.com/media/*"]
+},
+          ["blocking", "responseHeaders"])
